07f216b51a3cffa171185229ffe2b07c42b93ec6
pratamawijaya <set.mnemonix@gmail.com>
2021-03-13 20:16:05 +0700

    fix build

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Theme.kt
@@ -46,7 +46,6 @@ private val DarkColorPalette = darkColors(
     onSurface = white85
 )
 
-
 @Composable
 fun MyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
     val colors = if (darkTheme) {

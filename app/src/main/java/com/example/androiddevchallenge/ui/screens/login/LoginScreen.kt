ab7862de280a34001a82209c2d6a141eb3eb5827
pratamawijaya <set.mnemonix@gmail.com>
2021-03-14 08:55:14 +0700

    login screen

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/login/LoginScreen.kt
@@ -16,28 +16,95 @@
 package com.example.androiddevchallenge.ui.screens.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.ui.theme.MySecondaryTheme
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 @Composable
 fun LoginScreen(loginSuccessListener: () -> Unit) {
-    Surface {
+    var emailAddress by mutableStateOf("")
+    var password by mutableStateOf("")
+
+
+    Surface(modifier = Modifier.fillMaxHeight()) {
         Column(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier.padding(horizontal = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "Log in with email",
-                modifier = Modifier.paddingFromBaseline(top = 184.dp)
+                modifier = Modifier.paddingFromBaseline(top = 184.dp),
+                style = MaterialTheme.typography.h1
             )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = emailAddress,
+                onValueChange = { emailAddress = it },
+                placeholder = {
+                    Text(text = "Email Address", style = MaterialTheme.typography.body1)
+                })
+            Spacer(modifier = Modifier.height(8.dp))
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = password,
+                onValueChange = { password = it },
+                placeholder = {
+                    Text(text = "Password (8+ characters)", style = MaterialTheme.typography.body1)
+                })
+            Text(
+                buildAnnotatedString {
+                    append("By clicking below, you agree to our ")
+                    withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                        append("Terms of Use")
+                    }
+                    append(" and consent to our ")
+                    withStyle(style = SpanStyle(textDecoration = TextDecoration.Underline)) {
+                        append("Privacy Policy")
+                    }
+                    append(".")
+                },
+                modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 16.dp),
+                style = MaterialTheme.typography.body2,
+                textAlign = TextAlign.Center
+            )
+            MySecondaryTheme {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .paddingFromBaseline(bottom = 8.dp)
+                        .fillMaxWidth()
+                        .height(48.dp)
+                ) {
+                    Text(text = stringResource(R.string.log_in))
+                }
+            }
         }
     }
 }
